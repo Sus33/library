465aa9e4d8a32d61cc4b3a140079e23c0d0faee3
Sus33 <susannamkrtchan98@gmail.com>
2018-08-24 01:12:24 +0400

    Added Author

---
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -0,0 +1,30 @@
+package com.example.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@SpringBootApplication
+public class LibraryApplication extends WebMvcConfigurerAdapter {
+
+    public static void main(String[] args) {
+        SpringApplication.run (LibraryApplication.class, args);
+    }
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+    @Bean
+    public ViewResolver internalResourceViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
+}
